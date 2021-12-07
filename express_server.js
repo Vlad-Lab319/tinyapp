ef42a305f499b82e1c6f178530be79e1117b5c42
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-07 03:45:18 +0000

    POST request adds new object to database

--- express_server.js
+++ express_server.js
@@ -33,6 +33,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   console.log(generateRandomString());
+  urlDatabase[generateRandomString()] = req.body.longURL;
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
--- run.js
+++ run.js
@@ -1,8 +1,21 @@
 let randString = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
-let randString2 = Math.random().toString(36).replace(/g+/[^a-z]+/g, '').substr(0, 6);
+let randString2 = Math.random().toString(36).replace(/g+/[^a-z], '').substr(0, 6);
 let randString3 = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
 
 
 console.log(randString);
 console.log(randString2);
 console.log(randString3);
+
+// function makeid(length) {
+//   var result           = '';
+//   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//   var charactersLength = characters.length;
+//   for ( var i = 0; i < length; i++ ) {
+//     result += characters.charAt(Math.floor(Math.random() * 
+// charactersLength));
+//  }
+//  return result;
+// }
+
+// console.log(makeid(5));
