177bde4fe01d25e5a4495fa80e84e978d20143b7
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-08 01:02:09 +0000

    Add detailed comments

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+// Dummy database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,21 +17,25 @@ const urlDatabase = {
 //   res.send("Hello!");
 // });
 
+
+// Add new links pair
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Display all links pairs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Show certain pair details
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-// Add new link 
+// Add new set of links 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   let shortURLGenerated = generateRandomString();
@@ -39,7 +44,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURLGenerated}`);
 });
 
-// Update link
+// Update long link
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
@@ -51,19 +56,21 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 
-
+// Redirecting to long URL 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  console.log(longURL);
+  // console.log(longURL);
   res.redirect(longURL);
 });
 
+// Delete from database
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect('/urls');
 });
 
+// Random string generator
 function generateRandomString() {
   let randomString =  Math.random().toString(36).replace(/\W+/, '').substr(0, 6);
   return randomString;
